6fcc88bf5379cd658bb7ed1ea7ec6a6ecb3241d9
Michael Jester <m.jester.93@gmail.com>
2020-10-26 17:20:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function bfs(rootNode, vertices, edges){
+  
+}
 
+
+const findAdjacent(nodeName, vertices, edges) {
+  const filtered_edges = edges.filter( edge => {
+    return edge.includes(nodeName);
+  })
+
+  const mapped_filtered_edges = filtered_edges.map( name => {
+    return findNode(node !== nodeName);
+  })[0].map( name => {
+    return findNode(name, vertices)
+  }).filter( node => {
+    return node.distance == null;
+  })
 }
